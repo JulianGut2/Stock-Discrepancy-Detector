5cff575773c58a318cedc287ab135f5508674752
Julian Gutierrez <157057593+JulianGut2@users.noreply.github.com>
2026-03-04 19:15:47 -0600

    Added fetch stock data function

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,11 @@
 import yfinance as yf
 
-data = yf.download("AAPL", period = "5y", auto_adjust = True, progress = False)
-
-print(f"{data.columns} \n\n {data.shape}")
+def fetch_stock_data(stock, period = "5y"):
+        data = yf.download(stock, period = period, auto_adjust = True, progress = False)
+        if data.empty:
+            return None, "Stock not found"
+        else:
+            data.columns = data.columns.get_level_values(0)
+            data["Return"] = data["Close"].pct_change()
+            data = data.dropna()
+            return data, None
