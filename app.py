bbbb922d4bd127e55338343ff70947d5df23ba96
Julian Gutierrez <157057593+JulianGut2@users.noreply.github.com>
2026-03-04 22:45:34 -0600

    created streamlit file

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import yfinance as yf
+from src.data_loader import fetch_stock_data
+from src.disrepancy_detector import detect_zscore, detect_bollinger, detect_volume_spike, detect_iqr, combine_signals
+from src.utils import get_anomaly_summary
+
+st.set_page_config(page_title = "Stock Discrepancy Detector", layout = "wide")
